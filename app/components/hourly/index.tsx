12b8b4539452cfc5fa98dd5ee854c8eb4040c5cc
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-05 02:34:08 +0300

    Remove hour selection, refactor styles

--- app/components/hourly/index.tsx
+++ app/components/hourly/index.tsx
@@ -6,24 +6,26 @@ import { beaufortScale, weatherCodes } from '../../lib/utils'
 import styles from './hourly.module.css'
 import HourlyDropdown from '@components/hourly-dropdown';
 
-// function HourlyDropdownHour({onChange}) {
-//   let hours = [];
-//   for (let i = 0; i < 24; i++) { hours.push(i) }
-//   return (
-//     <>
-//     <label htmlFor="hourlyHour">Select hour: </label>
-//     <select name="hourlyHour" id="hourlyHour" onChange={onChange}>
-//       {hours.map(hour => <option value={hour} key={hour}>{hour}</option> )}
-//     </select>
-//   </>
-//   )
-// }
+type HourlyProps = {
+  hourlyData: {
+    time: string[],
+    temperature_2m: number[],
+    relativehumidity_2m: number[],
+    weathercode: number[],
+    cloudcover: number[],
+    visibility: number[],
+    windspeed_10m: number[],
+    winddirection_10m: number[],
+    is_day: (0 | 1)[],
+    uv_index: number[]
+  },
+  currentHour: number
+}
 
-function Hourly({ hourlyData, currentHour }) {
+function Hourly({ hourlyData, currentHour }: HourlyProps) {
   let today = new Date(hourlyData.time[0])
   const todayName = today.toLocaleString('en-us', { weekday: 'long' })
   const [selectedDay, setSelectedDay] = useState(todayName)
-  // const [selectedHour, setSelectedHour] = useState('0')
 
   let days = {};
   for (let i = 0; i < hourlyData.time.length; i += 24) {
@@ -35,48 +37,41 @@ function Hourly({ hourlyData, currentHour }) {
     setSelectedDay(e.target.value)
   }
 
-  // function handleHourSelection(e: { target: { value: SetStateAction<string>; }; }) {
-  //   console.log(e.target.value)
-  //   setSelectedHour(e.target.value)
-  // }
-
-
   return (
-    <section style={{width: '100%', textAlign: 'center'}}>
-      <h3>Hour by Hour</h3>
+    <section className={styles.hourlyWrapper}>
+      <h3 className={styles.hourlyTitle}>Hour by Hour</h3>
       <HourlyDropdown days={Object.keys(days)} onChange={handleSelection} />
-      {/* <HourlyDropdownHour onChange={handleHourSelection} /> */}
       {
-        days[selectedDay].map((hour: string, i: number) => {
-            const d = new Date(hour);
-            const hourNumber = d.getHours();
-            const index = hourlyData.time.findIndex((time: string) => time === hour)
-            // const weatherDescription = `${weatherCodes[hourlyData.weathercode[index]].description[0].toUpperCase()}${weatherCodes[hourlyData.weathercode[index]].description.slice(1)}`
-            return (
-              selectedDay === todayName ? 
-              currentHour <= hourNumber &&
-              <div className={styles.hourly} key={hour}>
-                <HourlyItem 
-                  hourlyData={hourlyData}
-                  index={index}
-                  hour={d.toLocaleString('en-us', {hour: 'numeric'})}
-                />
-              </div>
-              : 
-              <div className={styles.hourly} key={hour}>
-                <HourlyItem 
-                  hourlyData={hourlyData}
-                  index={index}
-                  hour={d.toLocaleString('en-us', {hour: 'numeric'})}
-                />
-              </div>            
-            )
-          })}
+        days[selectedDay].map((hour: string) => {
+          const d = new Date(hour);
+          const hourNumber = d.getHours();
+          const index = hourlyData.time.findIndex((time: string) => time === hour)
+          // const weatherDescription = `${weatherCodes[hourlyData.weathercode[index]].description[0].toUpperCase()}${weatherCodes[hourlyData.weathercode[index]].description.slice(1)}`
+          return (
+            selectedDay === todayName ?
+            currentHour <= hourNumber &&
+            <div className={styles.hourly} key={hour}>
+              <HourlyItem
+                hourlyData={hourlyData}
+                index={index}
+                hour={d.toLocaleString('en-us', { hour: 'numeric' })}
+              />
+            </div>
+            :
+            <div className={styles.hourly} key={hour}>
+              <HourlyItem
+                hourlyData={hourlyData}
+                index={index}
+                hour={d.toLocaleString('en-us', { hour: 'numeric' })}
+              />
+            </div>
+          )
+        })}
     </section>
   )
 }
 
-export function HourlyItem({hourlyData, index, hour}) {
+export function HourlyItem({ hourlyData, index, hour }) {
   const weatherDescription = `${weatherCodes[hourlyData.weathercode[index]].description[0].toUpperCase()}${weatherCodes[hourlyData.weathercode[index]].description.slice(1)}`
   const iconName = hourlyData.is_day[index] ? weatherCodes[hourlyData.weathercode[index]].iconNameDay : weatherCodes[hourlyData.weathercode[index]].iconNameNight
   const temperature = Math.round(hourlyData.temperature_2m[index])
@@ -84,13 +79,13 @@ export function HourlyItem({hourlyData, index, hour}) {
   const cloudcover = hourlyData.cloudcover[index]
   const winddirection = hourlyData.winddirection_10m[index]
   const windspeed = Math.round(hourlyData.windspeed_10m[index])
-  const visibility = hourlyData.visibility[index]
+  const visibility = hourlyData.visibility[index] / 1000 // To convert meters to kilometers
   const uv_index = hourlyData.uv_index[index]
   
   return (
-    <div className={styles.hourlyBox} id={index}>
-      <h2 className={styles.hourlyTitle}>{hour}</h2> 
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+    <div className={styles.hourlyItemBox} id={index} data-testid="hourlyItem">
+      <h2 className={styles.hourlyItemHour}>{hour}</h2>
+      <div className={styles.hourlyItemContainer}>
         <Image 
           src={`/weather-icons/${iconName}.svg` }
           alt={`${weatherDescription} icon`}
@@ -98,11 +93,11 @@ export function HourlyItem({hourlyData, index, hour}) {
           height={100}
         />
         <div>
-          <p>{temperature}<span style={{fontSize: 'smaller'}}>°C</span></p>
+          <p>{temperature}<span className={styles.small}>°C</span></p>
           <p>{weatherDescription}</p>
         </div>
       </div>
-      <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
+      <div className={styles.hourlyItemRow}>
       <div className={styles.miscItem}>
         <Image src="./misc-icons/humidity.svg" alt="humidity icon" width={40} height={40}/>
         <p>Humidity:</p>
@@ -114,7 +109,7 @@ export function HourlyItem({hourlyData, index, hour}) {
         <p>{cloudcover}%</p>
       </div>
       </div>
-      <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
+      <div className={styles.hourlyItemRow}>
         <div className={styles.miscItem}>
           <Image 
             src="./misc-icons/compass.svg" 
@@ -137,11 +132,11 @@ export function HourlyItem({hourlyData, index, hour}) {
           <p>{windspeed} mph: {beaufortScale(windspeed).description}</p>
         </div>
       </div>
-      <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
+      <div className={styles.hourlyItemRow}>
         <div className={styles.miscItem}>
           <Image src="./misc-icons/visibility.svg" alt="visibility (mist) icon" width={40} height={40}/>
           <p>Visibility:</p>
-          <p>{visibility} meters</p>
+          <p>{visibility} kms</p>
         </div>
         <div className={styles.miscItem}>
           <Image 
