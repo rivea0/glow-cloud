1e48696bcb73960bed46f9b3832f262556e74d78
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-05 02:16:58 +0300

    Change icons to ts files, add menu icon

--- app/components/icons/index.ts
+++ app/components/icons/index.ts
@@ -1,6 +1,6 @@
 export { default as Sun } from './sun'
 export { default as Moon } from './moon'
+export { default as Menu } from './menu'
 export { default as Link } from './link'
 export { default as Mail } from './mail'
 export { default as GitHub } from './github'
-
