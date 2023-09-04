9ab09855d62198d3686469cf03f70c822616a623
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-05 02:44:43 +0300

    Remove Averia_Libre font, make it a client component to use HeaderMenu

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,19 +1,31 @@
-import styles from './header.module.css';
-import Link from 'next/link';
-import { Averia_Libre } from 'next/font/google'
+'use client'
+
+import styles from './header.module.css'
+import Link from 'next/link'
+import { DM_Sans } from 'next/font/google'
+import { useState } from 'react'
 import Logo from '@components/logo'
 import ThemeSwitcher from '@components/theme-switcher'
-
-const averiaLibre = Averia_Libre({ subsets: ['latin'], weight: ['300', '700'] })
-
+import HeaderMenu from '@components/header-menu'
+import { Menu } from '@components/icons'
+const dm_sans = DM_Sans({ subsets: ['latin'], weight: '300' })
 
 export default function Header() {
+
+  const [show, setShow] = useState(false)
+
+  function handleClick() {
+    setShow(prevShow => !prevShow)
+  }
+
   return (
-    <div className={`${styles.header} ${averiaLibre.className}`}>
+    <div className={`${styles.header} ${dm_sans.className}`}>
       <Link href='/'><Logo /></Link>
       <div className={styles.right}>
         <ThemeSwitcher />
-        <Link href='/about' className={styles.about}>What is this site?</Link>
+        <HeaderMenu icon={<Menu strokeWidth={2} />} onClick={handleClick}>
+          {show && <div className={styles.aboutText}><Link href='/about' className={styles.about}>What is this site?</Link></div>}
+        </HeaderMenu>
       </div>
   </div>
   )
