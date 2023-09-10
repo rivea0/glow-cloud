cefd5cd64083ebed2b9b3cad028e678f8b4cba39
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-10 16:16:37 +0300

    Add custom error page

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Image from 'next/image'
+import GoBackButton from '@components/go-back-button'
+import styles from '@styles/not-found.module.css'
+
+export default function NotFound() {
+  return (
+    <div className={styles.errorContainer}>
+      <div className={styles.error}>
+        <span className={styles.span}>4</span>
+        <Image src="/eye.gif" alt="eye gif" width={100} height={100}></Image>
+        <span className={styles.span}>4</span>
+      </div>
+      <p>Page not found.</p>
+      <GoBackButton />
+    </div>
+  )
+}
