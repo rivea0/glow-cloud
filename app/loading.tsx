99a09f9fe82aeea956edc469fbd8c2fb7c4a5a88
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-09-10 17:38:32 +0300

    Add loading component

---
+++ app/loading.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from '@styles/loading.module.css'
+
+export default function Loading() {
+  return (
+    <div className={styles.loadingContainer}>
+      <Image 
+        src='/misc-icons/loading.svg' 
+        alt="loading animated icon" 
+        width={75} 
+        height={75}
+      >
+      </Image>
+    </div>
+  )
+}
