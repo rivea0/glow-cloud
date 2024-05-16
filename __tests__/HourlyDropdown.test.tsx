572215f275c7445a847ec4ff9184119d61f725df
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-05-17 01:38:09 +0300

    Rename `HourlyDropdown` to `DaysDropdown`, add `HoursDropdown`, add types to props

--- __tests__/HourlyDropdown.test.tsx
+++ __tests__/HourlyDropdown.test.tsx
@@ -1,18 +1,18 @@
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import HourlyDropdown from '@components/hourly-dropdown'
+import { DaysDropdown } from '@components/hourly-dropdown'
 
 const noop = () => { }
 
 const days = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday']
 
-describe('HourlyDropdown', () => {
+describe('DaysDropdown', () => {
   it('renders', () => {
-    render(<HourlyDropdown days={days} onChange={noop} />)
+    render(<DaysDropdown days={days} onChange={noop} />)
   })
 
   it('shows the dropdown', () => {
-    render(<HourlyDropdown days={days} onChange={noop} />)
+    render(<DaysDropdown days={days} onChange={noop} />)
 
     expect(screen.getByLabelText('Select day:')).toBeInTheDocument()
   })
--- app/components/hourly-dropdown/index.tsx
+++ app/components/hourly-dropdown/index.tsx
@@ -1,8 +1,43 @@
 import styles from './hourly-dropdown.module.css'
 
-export default function HourlyDropdown({ days, onChange }) {
+export function HoursDropdown({
+  hours,
+  onChange,
+  selectedHour,
+}: {
+  hours: string[]
+  onChange: (e: { target: { value: string } }) => void
+  selectedHour: string
+}) {
   return (
-    <>
+    <div className={styles.dropdownItem}>
+      <label htmlFor="hourlyHour">Jump to hour: </label>
+      <select
+        name="hourlyHour"
+        id="hourlyHour"
+        onChange={onChange}
+        className={styles.dropdown}
+        value={selectedHour}
+      >
+        {hours.map((hour) => (
+          <option value={hour} key={hour}>
+            {hour}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export function DaysDropdown({
+  days,
+  onChange,
+}: {
+  days: string[]
+  onChange: (e: { target: { value: string } }) => void
+}) {
+  return (
+    <div>
       <label htmlFor="hourlyDay">Select day: </label>
       <select
         name="hourlyDay"
@@ -10,8 +45,12 @@ export default function HourlyDropdown({ days, onChange }) {
         onChange={onChange}
         className={styles.dropdown}
       >
-        {days.map((day: string) => <option value={day} key={day}>{day}</option> )}
+        {days.map((day) => (
+          <option value={day} key={day}>
+            {day}
+          </option>
+        ))}
       </select>
-    </>
+    </div>
   )
 }
