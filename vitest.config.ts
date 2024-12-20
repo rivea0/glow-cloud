0aec18c896048bfc87f6bd5fd7f075e3dd751143
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-12-21 01:55:47 +0300

    Fix type error in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+export default {
   plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     setupFiles: './__tests__/setup.ts',
   },
-})
+}
