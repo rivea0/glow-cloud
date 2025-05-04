6334d9bc0a51bd29bffe1aba3eee3e579d4532ea
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-05-04 12:40:13 +0300

    Add simple test for Header

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -1,1 +1,24 @@
-// TODO
+import { describe, it, expect, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Header from '@components/header'
+
+describe('Header', () => {
+  it('renders', () => {
+    render(<Header />)
+  })
+
+  it ('displays logo text', () => {
+    render(<Header />)
+
+    vi.mock('next/font/google', () => ({
+      DM_Sans: () => ({
+        style: {
+          fontFamily: 'mocked',
+        },
+      }),
+    }))
+
+    expect(screen.getByText('glow cloud*')).toBeInTheDocument()
+    expect(screen.getByText('*all hail')).toBeInTheDocument()
+  })
+})
